a397786f232b4c4d93f00b0cfa8580749b70f0d4
tommydrozd <tomekdro@gmail.com>
2024-06-16 16:44:52 +0200

    final?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,73 @@
-function App() {
-  return (
-    <>
-      <h1>GOIT-React-HW-02-PhoneBook (Vite)</h1>
-    </>
-  );
-}
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/Form';
+import { ContactList } from './ContactList/List';
+import { Filter } from './Filter/Filter';
+import s from "./AllStyles.module.scss";
 
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      filter: '',
+    };
+  }
+
+  addContact = event => {
+    const loweredCase = event.name.toLowerCase().trim();
+
+    const exists = this.state.contacts.some(
+      contact => contact.name.toLowerCase().trim() === loweredCase
+    );
+
+    if (exists) {
+      alert(`${event.name} is already in contacts!`);
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [...contacts, event],
+      }));
+    }
+  };
+
+  addFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  filteredContacts = () => {
+    const { filter, contacts } = this.state;
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteContact = id =>
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== id),
+    }));
+
+  render() {
+    const { filter } = this.state;
+
+    return (
+      <section className={s.content}>
+        <div className={s.content__container}>
+          <ContactForm addContact={this.addContact} />
+          <ContactList
+            contacts={this.filteredContacts()}
+            deleteContact={this.deleteContact}
+          >
+            <Filter filter={filter} addFilter={this.addFilter} />
+          </ContactList>
+        </div>
+      </section>
+    ); 
+  }
+
+}
 export default App;
